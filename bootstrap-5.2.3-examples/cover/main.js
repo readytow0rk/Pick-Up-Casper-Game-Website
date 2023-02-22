fc575f12f316eb7295f78bb82172c2a0aa62800b
Nikita <nmal4ik@gmail.com>
2023-02-22 10:14:48 +0000

    wrote unction for game, took design from bootstrap

--- bootstrap-5.2.3-examples/cover/main.js
+++ bootstrap-5.2.3-examples/cover/main.js
@@ -8,10 +8,10 @@ const score = document.querySelector('#score')
 const start = document.querySelector('.btn.btn-lg.btn-secondary.fw-bold.border-white.bg-white')
 const h = document.querySelector('h1')
 const txt = document.querySelector('.lead')
-const refresher = document.querySelectorAll('.nav-link.fw-bold.py-1.px-0')
-const color = document.querySelectorAll('.nav-link.fw-bold.py-1.px-0')
-const body = document.querySelectorAll('d-flex.h-100.text-center.text-bg-dark')
+let body = document.querySelector('body')
 const colorFirst = document.querySelector('.changeColorQ')
+let colorBtn = document.querySelector('#colorCall')
+
 
 // Hide text and show game
 start.addEventListener('click', () => {
@@ -40,19 +40,19 @@ function randomSquare(){
     hitPosition = randomSquare.id
 }
 
-// square.forEach(squaresInside => {
-//     squaresInside.addEventListener('mousedown'() => {
-//         if (squaresInside.id == hitPosition){
-//             result++
-//             score.textContent = result
-//             hitPosition = null     
-//         }
-//     })
-// })
+square.forEach(squaresInside => {
+    squaresInside.addEventListener('click', () => {
+        if (squaresInside.id == hitPosition){
+            result++
+            score.textContent = result
+            hitPosition = null     
+        }
+    })
+})
 
 
 function moveMole() { 
-    timerId = setInterval(randomSquare, 1000)
+    timerId = setInterval(randomSquare, 300)
 }
 
 moveMole()
@@ -73,5 +73,34 @@ function countDown() {
 let countDownTimerId = setInterval(countDown, 1000)
      
 });
+
+
+
+
+// try 1
+colorBtn.addEventListener('click',()=>{
+if (body.style.boxShadow = 'rgba(0, 0, 0, 0.5) 0px 0px 80px 0px inset') {
+body.style.boxShadow = 'rgba(255, 255, 255, 0.5) 0px 0px 80px 0px inset'
+}
+else if (body.style.boxShadow  = 'rgba(255, 255, 255, 0.5) 0px 0px 80px 0px inset') {
+body.style.boxShadow  = 'rgba(0, 255, 221, 0.5) 0px 0px 80px 0px inset'
+}
+else {
+    body.style.boxShadow  = 'rgba(232, 105, 252, 0.5) 0px 0px 80px 0px inset'
+  }
+})
+// try 2
+
+
+
+
+    
+// let myelement = document.querySelector('body')
+
+// let boxShadow = window.getComputedStyle(body).boxShadow
+
+// console.log(boxShadow)
+
+
 
 
